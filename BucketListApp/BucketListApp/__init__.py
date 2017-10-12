3097544b52e5ad1a000f244560a6d4a5a5290baf
Kevin Channon <kevinchannon@gmail.com>
2017-10-12 22:51:46 +0100

    Getting MySQL credentials from file

--- BucketListApp/BucketListApp/__init__.py
+++ BucketListApp/BucketListApp/__init__.py
@@ -2,22 +2,42 @@
 The flask application package.
 """
 
+import os
+import json
 from flask import Flask
-from flask.ext.mysql import MySQL
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 mysql = MySQL()
 
 def init_mysql():
     # open credentials file and edit mysql.config
-    pass
 
-#MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = ''
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = ''
-app.config['MYSQL_DATABASE_HOST'] = ''
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    credentials_path = os.path.join( SITE_ROOT, "Credentials.json")
 
-mysql.init_app(app)
+    print "Looking for DB credentials in ", credentials_path
+
+    with open(credentials_path) as credentials_file:
+        json_string = credentials_file.read()
+        credentials = json.loads(json_string)
+        if "user" not in credentials:
+            return False
+        if "password" not in credentials:
+            return False
+        if "database" not in credentials:
+            return False
+        if "host" not in credentials:
+            return False
+
+        #MySQL configurations
+        app.config['MYSQL_DATABASE_USER'] = credentials["user"]
+        app.config['MYSQL_DATABASE_PASSWORD'] = credentials["password"]
+        app.config['MYSQL_DATABASE_DB'] = credentials["database"]
+        app.config['MYSQL_DATABASE_HOST'] = credentials["host"]
+
+        return True
+
+    mysql.init_app(app)
 
 import BucketListApp.views
--- BucketListApp/runserver.py
+++ BucketListApp/runserver.py
@@ -4,14 +4,16 @@ This script runs the BucketListApp application using a development server.
 
 from os import environ
 
-from BucketListApp import app
+from BucketListApp import app, init_mysql
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
+    if init_mysql():
+        HOST = environ.get('SERVER_HOST', 'localhost')
+        try:
+            PORT = int(environ.get('SERVER_PORT', '5555'))
+        except ValueError:
+            PORT = 5555
 
-    app.run(HOST, PORT)
-#    app.run()
+        app.run(HOST, PORT)
+    else:
+        print "Failed to intialise DB"
