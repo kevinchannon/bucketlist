f89b25b7d410bf7633818550399f7bdac6fe484c
Kevin Channon <kevinchannon@gmail.com>
2017-10-18 22:26:52 +0100

    Updating venv due to new python exe

--- BucketListApp/BucketListVenv/Scripts/activate.bat
+++ BucketListApp/BucketListVenv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\dev\repos\personal\BucketListApp\BucketListVenv"
+set "VIRTUAL_ENV=c:\dev\repos\personal\BucketListApp\BucketListVenv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
